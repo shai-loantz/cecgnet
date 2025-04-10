ea5de876ca8bf4daeb20eccd8e814fd0bfcf256d
Shai <shai.loantz@campus.technion.ac.il>
2025-04-10 18:07:20 +0300

    data module log

--- data_tools/data_module.py
+++ data_tools/data_module.py
@@ -3,6 +3,7 @@ from torch.utils.data import random_split, DataLoader
 
 from data_tools.data_set import ECGDataset
 from settings import DataConfig, PreprocessConfig
+from utils.logger import logger
 
 
 class DataModule(LightningDataModule):
@@ -12,6 +13,7 @@ class DataModule(LightningDataModule):
         length = len(data_set)
         valid_size = int(length * data_config.validation_size)
         train_size = length - valid_size
+        logger.info(f'Train set size: {train_size}, Validation set size: {valid_size}')
 
         self.data_loader_config = data_config.get_data_loader_config()
         self.train_dataset, self.val_dataset = random_split(data_set, [train_size, valid_size])
