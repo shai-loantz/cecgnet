4ef33dfff7e7870246c99cbc433fb80d201ae69e
Shai <shai.loantz@campus.technion.ac.il>
2025-04-10 18:09:30 +0300

    circular imports

--- utils/ddp.py
+++ utils/ddp.py
@@ -1,7 +1,6 @@
 import os
-import torch.distributed as dist
 
-from utils.logger import logger
+import torch.distributed as dist
 
 
 def is_main_proc():
@@ -10,17 +9,3 @@ def is_main_proc():
 
 def should_log():
     return is_main_proc() or dist.is_initialized()
-
-
-def get_run_id() -> str:
-    try:
-        with open('RUN_ID', 'r') as fh:
-            return fh.read()
-    except Exception:
-        logger.exception('Could not get run id. Using placeholder')
-        return 'RUN_ID_PLACEHOLDER'
-
-
-def set_run_id(run_id: str) -> None:
-    with open('RUN_ID', 'w') as fh:
-        fh.write(run_id)
---
+++ utils/run_id.py
@@ -0,0 +1,15 @@
+from utils.logger import logger
+
+
+def get_run_id() -> str:
+    try:
+        with open('RUN_ID', 'r') as fh:
+            return fh.read()
+    except Exception:
+        logger.exception('Could not get run id. Using placeholder')
+        return 'RUN_ID_PLACEHOLDER'
+
+
+def set_run_id(run_id: str) -> None:
+    with open('RUN_ID', 'w') as fh:
+        fh.write(run_id)
