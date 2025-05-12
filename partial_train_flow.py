dcf0d23d56c2b97254f9d300b67496671de63f05
tal2tal2 <tal4tal4@gmail.com>
2025-05-12 14:55:55 +0300

    wandb sweep initial

--- partial_train_flow.py
+++ partial_train_flow.py
@@ -4,10 +4,9 @@ from lightning import seed_everything
 
 from models import MODELS
 from settings import Config
-from utils.tools import train, test, restart_wandb_run
+from utils.tools import train, test, start_wandb_sweep, restart_wandb_run
 
 seed_everything(42)
-config = Config()
 RUN_POSTFIX = secrets.token_hex(2)
 
 
@@ -16,7 +15,11 @@ def main() -> None:
     This is a partial and temporary flow to compare models.
     This flow consists of training on the "pre-training" data set and then testing on the "fine-tuning" database.
     """
-    restart_wandb_run(config, RUN_POSTFIX)
+    config = Config()
+    if config.manual_config:
+        restart_wandb_run(config, RUN_POSTFIX)
+    else:
+        config = start_wandb_sweep(config, RUN_POSTFIX)
     model = MODELS[config.model_name](config.pre_model)
     train(model, config, use_pretraining=True)
     test(config)
